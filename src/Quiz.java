2dbfddc92b80c5486385fb92f66daeee78f65d7b
748674 <748674@123-PC16.student.Tor.Loc>
2019-05-21 16:43:13 -0400

    initial logic

---
+++ src/Quiz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Instructors infected in number of weeks
+		Scanner keyboard=new Scanner(System.in);
+		System.out.println(" the number of weeks you want to calculate the infection rate for");
+		int weeks=keyboard.nextInt();
+		
+		//declare variable
+		double instructors=0;
+		
+		
+		
+		//calculate how many instructors are infected at the end of each week
+		
+		
+		
+		
+		// output the total number of infected instructors
+
+	}
+
+}
